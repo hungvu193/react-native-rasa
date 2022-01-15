2f2e2ea0852c47df41cd0daddd3e70ce79bed3c6
jonra1993 <jon_ra@hotmail.es>
2022-01-15 17:56:08 -0500

    Add reset mesages  and restart

--- App.tsx
+++ App.tsx
@@ -7,16 +7,16 @@
  */
 
 import React, { useRef } from 'react';
-import { SafeAreaView, StatusBar, Button } from 'react-native';
+import { SafeAreaView, StatusBar, Text } from 'react-native';
 
-import RasaChat, { IRasaChatHandles } from './RNRasa';
+import RasaChat, { Send, InputToolbar, Composer, Actions, IRasaChatHandles } from './RNRasa';
 
 import styles from './styles';
 
 // your host http://localhost:5005. Apps work better on https so you can use ngrok if development
-const HOST = 'http://localhost:5005';
+const HOST = 'https://e35d-2800-bf0-24a-10f4-a59a-f843-534d-fa83.ngrok.io';
 
-// Your bot
+// Avatar images
 const botAvatar = "https://images.unsplash.com/photo-1485827404703-89b55fcc595e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
 const userAvatar = "https://images.unsplash.com/photo-1483884105135-c06ea81a7a80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
 
@@ -26,19 +26,18 @@ const userAvatar = "https://images.unsplash.com/photo-1483884105135-c06ea81a7a80
 const App = () => {
   const rasaChatRef = useRef<IRasaChatHandles>(null);
 
-  const clearMessages = () => {
+  const resetMessages = () => {
     rasaChatRef?.current?.resetMessages();
+  }
+
+  const resetBot = () => {
+    rasaChatRef?.current?.resetBot();
   }
 
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={styles.container}>
-        <Button
-          onPress={clearMessages}
-          title="Clear messages"
-          color="#2097F3"
-        />
         <RasaChat
           ref={rasaChatRef}
           host={HOST}
@@ -46,6 +45,49 @@ const App = () => {
           botAvatar={botAvatar}
           userAvatar={userAvatar}
           showUserAvatar
+          renderInputToolbar={(props) => (
+            <InputToolbar
+              {...props}
+              containerStyle={{
+                // backgroundColor: '#d4d4d8',
+                paddingTop: 3,
+                paddingBottom: 3,
+                marginTop: 10,
+                marginBottom: -2
+
+              }}
+              primaryStyle={{ alignItems: 'center' }}
+            />
+          )}
+          renderActions={(props) => (
+            <Actions
+              {...props}
+              containerStyle={styles.containerActions}
+              options={{
+                'Clear messages': resetMessages,
+                'Reset Bot': resetBot,
+                'Cancel': () => {},
+              }}
+            />
+          )}
+          renderComposer={(props) => (
+            <Composer
+              {...props}
+              textInputStyle={styles.textComposer}
+            />
+          )}
+          alwaysShowSend
+          renderSend={(props) => {
+            return (
+              <Send
+                {...props}
+                disabled={!props.text}
+                containerStyle={styles.sendContainer}
+              >
+                <Text style={{color: !props.text ? '#d6d3d1' : '#2097F3'}}>Send</Text>
+              </Send >
+            );
+          }}
         />
       </SafeAreaView>
     </>
--- styles.ts
+++ styles.ts
@@ -1,5 +1,5 @@
-import {StyleSheet} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import { StyleSheet } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default StyleSheet.create({
   container: {
@@ -44,4 +44,26 @@ export default StyleSheet.create({
     paddingRight: 12,
     textAlign: 'right',
   },
+  textComposer: {
+    color: '#222B45',
+    width: 20,
+    backgroundColor: '#EDF1F7',
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#E4E9F2',
+    paddingTop: 8.5,
+    paddingHorizontal: 12,
+    marginLeft: 10,
+  },
+  containerActions: {
+    justifyContent: 'center',
+    marginBottom: 0,
+  },
+  sendContainer: {
+    width: 46,
+    height: 44,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 4,
+  }
 });
